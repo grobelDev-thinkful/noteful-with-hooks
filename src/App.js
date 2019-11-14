78634106bf0c25198d1c914fecff4c9ceb0d1420
grobelDev <grobeldev@gmail.com>
2019-11-13 16:52:57 -0800

    folder button navigation and filtering notes is working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
+import styled from 'styled-components'
 
 import dummyStore from './dummy-store.js';
 import HomePage from './pages/HomePage.js';
-import FolderPage from './pages/FolderPage.js';
+import SideBar from './components/SideBar.js';
+// import FolderPage from './pages/FolderPage.js';
 // import UserPage from './pages/UserPage';
+
+
 
 
 export default function App() {
 
-  // State Variables
-  const [data, setData] = useState(dummyStore);
-  const [currentFolder, setCurrentFolder] = useState('');
+  // Initial State
+  const [folders, setFolders] = useState(dummyStore.folders);
+  const [notes, setNotes] = useState(dummyStore.notes);
+  const [currentFolderId, setCurrentFolderId] = useState(false);
+
+  function renderSideBar() {
+    return (
+      <Switch>
+        <Route
+          exact path={["/", "/folder", "/folder/:folderId"]}
+          render={(props) => (
+            <SideBar
+              folders={folders}
+              currentFolderId={currentFolderId}
+              setCurrentFolderId={setCurrentFolderId}
+              {...props}
+            />
+          )}
+        />
+      </Switch>
+    )
+  }
+
+  function renderNotes() {
+    if (currentFolderId) {
+      let filteredNotes = notes.filter(note => note.folderId === currentFolderId);
+      return (
+        filteredNotes.map(note => (
+          <li>{note.name} - [{dummyStore.folders.find(folder => folder.id === note.folderId).name}]</li>
+        ))
+      )
+    } else {
+      return (
+        notes.map(note => (
+          <li>{note.name} - [{dummyStore.folders.find(folder => folder.id === note.folderId).name}]</li>
+        ))
+      );
+    }
 
 
+
+  }
+
+  // Key is using render instead of 'component'.
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route
-        exact path='/folder'
-        render={(props) => <FolderPage {...props} folders={data.folders} />}
-      />
-    </Switch>
+    <>
+      <Header>
+        <Link to="/" onClick={() => setCurrentFolderId(false)}>Noteful</Link>
+      </Header>
+      <Container>
+        <LeftContainer>
+          {renderSideBar()}
+        </LeftContainer>
+        <RightContainer>
+          <h1>Notes:</h1>
+          {renderNotes()}
+          {/* <ul>
+            {notes.map(note => (
+              <li>{note.name} - [{dummyStore.folders.find(folder => folder.id === note.folderId).name}]</li>
+            ))}
+          </ul> */}
+        </RightContainer>
+      </Container>
+    </>
   );
 }
+
+// CSS
+const Header = styled.header`
+  text-align: center;
+`;
+const Container = styled.div`
+  display: flex;
+`;
+const LeftContainer = styled.div`
+  flex-grow: 1;
+  padding: 5px;
+`;
+const RightContainer = styled.div`
+  flex-grow: 4;
+  padding: 5px;
+`;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SideBar(props) {
-  console.log('we got the folders');
-  console.log(props.folders);
+
+  const [folderId, setFolderId] = useState(props.currentFolderId);
+
+  useEffect(() => {
+    setFolderId(props.currentFolderId)
+  }, [props])
+
   return (
     <>
       <h1>SideBar</h1>
       <ul>
-        {props.folders.map((folder, i) => (
-          // <p>{JSON.stringify(folder)}</p>
-          <li>
-            <p>Folder {i}</p>
-            <p>Name: {folder.name}</p>
-          </li>
-        )
-        )}
+        {props.folders.map((folder, i) => (folder.id === folderId ?
+          (
+            <li>
+              <p>Folder!!! {i}</p>
+              <p>Name: {folder.name}</p>
+              <Link to={`/folder/${folder.id}`} onClick={() => props.setCurrentFolderId(folder.id)}>folder link</Link>
+            </li>
+          ) : (
+            <li>
+              <p>Folder {i}</p>
+              <p>Name: {folder.name}</p>
+              <Link to={`/folder/${folder.id}`} onClick={() => props.setCurrentFolderId(folder.id)}>folder link</Link>
+            </li>
+          )
+        ))}
       </ul>
     </>
   )
